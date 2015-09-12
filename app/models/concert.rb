4aa335208632f760cd227e54bd5ab334ccf9b9ee
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-12 13:36:37 +0200

    Added basic validations

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,2 +1,6 @@
 class Concert < ActiveRecord::Base
+	validates :band, :venue,:city,:date, :price, :description, presence: true
+	validates :description, length: { maximum: 2000 }
+	validates :band, length: { maximum: 100 }
+	validates :description, uniqueness: true
 end
