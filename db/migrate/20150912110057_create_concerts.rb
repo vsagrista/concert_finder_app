f7f4ec4bbf50861f027685387dba59e97b719a94
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-12 13:07:11 +0200

    Created model Concert. Run migration

---
+++ db/migrate/20150912110057_create_concerts.rb
@@ -0,0 +1,13 @@
+class CreateConcerts < ActiveRecord::Migration
+  def change
+    create_table :concerts do |t|
+      t.string :band
+      t.string :venue
+      t.string :city
+      t.datetime :date
+      t.integer :price
+      t.text :description
+      t.timestamps
+    end
+  end
+end
